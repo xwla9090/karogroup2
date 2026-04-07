617f6dbfba141ec46855640d800215dcaee23c9b
xwla9090 <Hawbirranya6@gmail.com>
2026-04-07 16:07:29 +0300

    remove realtime delay

---
+++ fix_remove_delay.py
@@ -0,0 +1,9 @@
+> Hawa:
+with open('src/RealtimeSync.js', encoding='utf-8') as f:
+    lines = f.readlines()
+
+lines[9] = ''
+
+with open('src/RealtimeSync.js', 'w', encoding='utf-8') as f:
+    f.writelines(lines)
+print('done!')
