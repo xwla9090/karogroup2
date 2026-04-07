d21a119bc9ac25e8e2dbbd802d3c36266331819b
xwla9090 <Hawbirranya6@gmail.com>
2026-04-07 18:17:25 +0300

    fix concrete dup useeffect

--- fix_concrete_dup2.py
+++ fix_concrete_dup2.py
@@ -1,7 +1,8 @@
 with open('src/App.js', encoding='utf-8') as f:
     lines = f.readlines()
 
-lines[2977] = ''
+for i in range(2897, 2907):
+    lines[i] = ''
 
 with open('src/App.js', 'w', encoding='utf-8') as f:
     f.writelines(lines)
