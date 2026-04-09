f799151c3e90f3f8c6032d3713406161d0b8d5d8
xwla9090 <Hawbirranya6@gmail.com>
2026-04-09 17:19:06 +0300

    fix claim bracket

---
+++ fix_claim_bracket.py
@@ -0,0 +1,8 @@
+with open('src/App.js', encoding='utf-8') as f:
+    lines = f.readlines()
+
+lines[3061] = '      window._karoLocal = false;\n    }\n  };\n'
+
+with open('src/App.js', 'w', encoding='utf-8') as f:
+    f.writelines(lines)
+print('done!')
--- src/App.js
+++ src/App.js
@@ -3059,6 +3059,8 @@ function ConcretePage({ t, s, isRtl, pKey, cashIQD, setCashIQD, cashUSD, setCash
       await supabase.from("concrete").upsert([{ id: updItem.id, project: pKey, date: updItem.date, currency: String(updItem.currency||"iqd"), meters: Number(updItem.meters||0), pricepermeter: Number(updItem.pricePerMeter||0), totalprice: Number(updItem.totalPrice||0), deposit: Number(updItem.deposit||0), depositpercent: Number(updItem.depositPercent||0), received: Number(updItem.received||0), isreceived: !!updItem.isReceived, depositclaimed: true, note: String(updItem.note||""), marked: !!updItem.marked, paidamount: Number(updItem.paidAmount||0), payments: JSON.stringify(updItem.payments||[]) }]);
       setItems(prev => prev.map(i => i.id === id ? { ...i, depositClaimed: true } : i));
       window._karoLocal = false;
+      window._karoLocal = false;
+    }
   };
 
 
